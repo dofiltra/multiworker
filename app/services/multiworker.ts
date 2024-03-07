985c0a00a2a3f9f805d974bdbe9429df16d69647
Mona Lisa <MonaLisa@mail>
2024-03-07 14:13:00 +0000

    added dprx-types

--- app/services/multiworker.ts
+++ app/services/multiworker.ts
@@ -1,24 +1,26 @@
-import path from "path";
+import { Dodecorator, type TResultError } from 'dprx-types'
+import path from 'path'
 
 export class Multiworker {
   static get rootPath() {
-    return path.resolve(import.meta.dir, "..");
+    return path.resolve(import.meta.dir, '..')
   }
 
-  static async createWorker({ filepath }: { filepath: string }) {
-    const workerURL = new URL(`../${filepath}`, import.meta.url).href;
-    const worker = new Worker(workerURL);
+  @Dodecorator.doretry({})
+  static async createWorker({ filepath }: { filepath: string }): TResultError<Worker> {
+    const workerURL = new URL(`../${filepath}`, import.meta.url).href
+    const worker = new Worker(workerURL)
 
-    worker.addEventListener("open", () => {
-      console.log("worker is open!");
-    });
-    worker.addEventListener("close", (event: CloseEvent) => {
-      console.log("worker is being closed");
-    });
-    worker.addEventListener("message", (event: MessageEvent) => {
-      console.log(event.data);
-    });
+    worker.addEventListener('open', () => {
+      console.log('worker is open!')
+    })
+    worker.addEventListener('close', (event: CloseEvent) => {
+      console.log('worker is being closed')
+    })
+    worker.addEventListener('message', (event: MessageEvent) => {
+      console.log(event.data)
+    })
 
-    return { result: worker };
+    return { result: worker }
   }
 }
